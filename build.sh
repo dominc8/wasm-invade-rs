29dd93bb12f8cd11b68826966d22cea177913a04
dominc8 <kormandominik@gmail.com>
2024-05-07 00:02:25 +0200

    move www directory and helper scripts out of rust dir project

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ set -eu
 TARGET=wasm32-unknown-unknown
 BINARY=target/$TARGET/release/invade_rs.wasm
 
+cd ./invade-rs
 cargo build --target $TARGET --release
 wasm-strip $BINARY
-mkdir -p www
-wasm-opt -o www/invade_rs.wasm -Oz $BINARY
+wasm-opt -o ../www/invade_rs.wasm -Oz $BINARY
+cd ..
 ls -lh www/invade_rs.wasm
